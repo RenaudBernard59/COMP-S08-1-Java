24faf731df07bd5cf2cbca85ba03cf7f1bbcd216
Renaud BERNARD <renaud.bernard@p59production.com>
2018-02-12 09:05:57 +0100

    Start
    Start the project.

--- Horloge/src/horloge/Horloge.java
+++ Horloge/src/horloge/Horloge.java
@@ -10,13 +10,35 @@ package horloge;
  * @author renob
  */
 public class Horloge {
+    //Instanaciation des variables
+    private int heure;
+    private int minute;
+    private int seconde;
     
+    //Constructeur
+    public void Personne(int heure, int minute, int seconde) {
+         
+    }
     
+    //Méthode afficher
+    public void afficher(int heure, int minute, int seconde) {
+        System.out.println(heure +":"+ minute +":"+ seconde);
+    }
     
+    //Méthode ajouterSeconde
+    public void ajouterSeconde(int heure, int minute, int seconde) {
+        seconde++;
+        if (seconde == 60) {
+            minute++;
+            seconde = 0;
+        } else if (minute == 60){
+            heure++;
+            minute = 0;
+        } else if (heure == 24) {
+            seconde++;
+            heure = 0;
+        }
+    }
     
-    
-    
-    
-    
-    
+       
 }
