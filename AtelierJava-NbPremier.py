4a0535bf4ed3631434d4a02c1b8ad7db51ed8e50
Renaud BERNARD <renaud.bernard@p59production.com>
2018-02-14 12:10:54 +0100

    Make a lot
    Make exercices of creation of array.
    Start exercice Nombres Premier  : Make UML diag, Java bogued script and Python for simple scripting test.

---
+++ AtelierJava-NbPremier.py
@@ -0,0 +1,35 @@
+i = 2
+tab = []
+while (i<=50):
+    j = 2
+    while (j<=50):
+        if (i>=j):
+            print("T=j="+str(j)+".i="+str(i))
+            #OK
+            mod = i%j
+            if (i%j !=0):
+                print("TT=i="+str(i)+".j="+str(j)+"Modulo="+str(mod))
+                #OK
+                if (j == i):
+                    print("TTT=j="+str(j)+".i="+str(i))
+                    #On est à la fin de la boucle
+                    tab.append(i)
+                    print("TAB:"+str(tab))
+                    i+=1
+                else:
+                    print("FFF=j="+str(j)+".i="+str(i))
+                    #NON on continue la boucle j
+                    j+=1
+                #END Else
+            else:
+                print("FF=i="+str(i)+".j="+str(j)+"Modulo="+str(mod))
+                #Est un nb divisible
+                i+=1
+            #END Else
+        else:
+            print("F=j="+str(j)+".i="+str(i))
+            #NON on trouve un nouveau nombre
+            i+=1
+        #END Else
+    #END while j
+#END while i
---
+++ AtelierJava/test/atelierjava/test/ListTest.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atelierjava.test;
+
+import com.sun.xml.internal.ws.message.stream.StreamAttachment;
+import java.util.ArrayList;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author renob
+ */
+public class ListTest {
+    
+    @Test
+    public void addAvecIndiceOK() {
+        ArrayList<Integer> nombresPremiers = new ArrayList();
+        nombresPremiers.add(2);
+        nombresPremiers.add(5);
+        nombresPremiers.add(1, 3);
+        
+        for(int nbp : nombresPremiers) {
+            System.out.println(nbp);
+        }
+    }/*END addAvecIndiceOK*/
+    
+    
+    
+    @Test
+    public void addOK() {
+        ArrayList<String> capitales = new ArrayList<>();
+        
+        capitales.add("Paris");
+        capitales.add("Madrid");
+        capitales.add("Rome");
+                
+        for(String cap : capitales) {
+            System.out.println(cap);
+        }
+            
+    }/*END addOK*/
+       
+    @Test
+    public void creerNombresPremiers() {
+        ArrayList<Integer> nbList = new ArrayList<>();
+        int i = 2;
+        while (i <= 50){
+            int j = 1;
+            while (j <= 50){
+            if (i >=j) {
+                if (j != 2) {
+                    if (i%j !=0) {
+                        if(i == j) {
+                            nbList.add(i);
+                        } else {
+                            j++;
+                        }/*END else4*/
+                    } else {
+                        i++;
+                    }/*END else3*/
+                } else {
+                    j++;
+                }/*END else2*/
+            } else {
+                i++;
+            }/*END else1*/
+            }/*END While j*/
+        }/*END While i*/
+        System.out.println(nbList);
+    }/*END creerNombresPremiers*/
+  
+    
+}/*END ListTest*/
